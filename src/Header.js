176d5803a54485556707831c3c3b61f1d814f3e7
christopher <coronac511@gmail.com>
2021-06-06 16:56:53 -0400

    pushing =

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header (){
+    return(
+        <div className="text">
+        <h1>Flatiron Car Rental</h1>
+        <p>Renting cars</p>
+        </div>
+    )
+}
+
+export default Header;
+
